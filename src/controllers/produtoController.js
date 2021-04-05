885500a181eb88053844fdea696a34a6bbb6aa5c
antonioneto00 <you@example.com>
2021-04-05 17:13:36 -0300

    Backend com Update e Delete

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -50,3 +50,53 @@ exports.SelectDetail = (req, res, next) => {
         })
         .catch(error => next(error));
 };
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const descricao = req.body.descricao;
+    const preco = req.body.preco;
+    const quantidadeEstoque = req.body.quantidadeEstoque;
+ 
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                produto.update({
+                    nome: nome,
+                    descricao: descricao,
+                    preco: preco,
+                    quantidadeEstoque: quantidadeEstoque,
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                produto.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
